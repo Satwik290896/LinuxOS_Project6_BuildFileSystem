346351603bc0386bf7edba0d21520db4bb469824
Satwik290896 <vsaisatwik1996@gmail.com>
2022-12-08 17:10:20 -0500

    mod_mount.sh

---
+++ mod_mount.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+make
+sudo umount -l /mnt/ez
+sudo losetup --detach /dev/loop0
+sudo rmmod myez
+sudo mkdir -p /mnt/ez
+dd bs=4096 count=400 if=/dev/zero of=./ez_disk.img
+sudo losetup --find --show ./ez_disk.img
+sudo ./format_disk_as_ezfs /dev/loop0
+sudo insmod -f ./myez.ko
+sudo mount -t myezfs /dev/loop0 /mnt/ez
